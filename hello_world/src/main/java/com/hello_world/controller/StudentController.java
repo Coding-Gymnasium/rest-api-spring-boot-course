02fe1208621f7556413a4e1a76357528b0b86920
Nico Rithner <nicorithner@gmail.com>
2024-02-17 14:46:47 -0700

    Use @Value annotation

--- hello_world/src/main/java/com/hello_world/controller/StudentController.java
+++ hello_world/src/main/java/com/hello_world/controller/StudentController.java
@@ -1,16 +1,20 @@
 package com.hello_world.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+// import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/student/")
 public class StudentController {
+    @Value("${app.name: Default Hello World}")
+    private String appName;
     @GetMapping("/get")
     // @RequestMapping(value = "/get", method = RequestMethod.GET)
     public String getStudent() {
-        return "Hello Student";
+        // return "Hello Student";
+        return appName;
     }
 }
