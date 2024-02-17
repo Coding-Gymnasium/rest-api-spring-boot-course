a882ee0ff0e1dc0e9a95472d1da167217f84da6b
Nico Rithner <nicorithner@gmail.com>
2024-02-17 14:15:02 -0700

    Add controller

--- hello_world/src/main/java/com/hello_world/hello_world/HelloWorldApplication.java
+++ hello_world/src/main/java/com/hello_world/hello_world/HelloWorldApplication.java
@@ -2,12 +2,15 @@ package com.hello_world.hello_world;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.hello_world.*")
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
+		System.out.println("Hey, hey, hey!!!");
 	}
 
 }
